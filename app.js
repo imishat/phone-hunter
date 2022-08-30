5a41b1c2a8e0f24f5ccd286b2c78409b8bdbadb5
MacBook <macbook@MacBooks-MacBook-Air.local>
2022-08-30 11:24:09 +0600

    add show more butten

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
-const lodePhone = async(searchText) => {
+const lodePhone = async(searchText, dataLimit) => {
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
     const res = await fetch(url);
     const data = await res.json()
-    displyPhone(data.data)
+    displyPhone(data.data, dataLimit)
 }
-const displyPhone = phones => {
+const displyPhone = (phones, dataLimit) => {
     console.log(phones)
     const phoneContainer = document.getElementById('phone-container')
     phoneContainer.textContent = ''
-        //displyPhone 20 ta
-    phones = phones.slice(0, 10)
-        //disply no phone found
+    const showAll = document.getElementById('show-all')
+    if (dataLimit && phones.length > 10) {
+        phones = phones.slice(0, 10)
+        showAll.classList.remove('d-none')
+
+    } else {
+        showAll.classList.add('d-none')
+    }
+
+    //displyPhone 20 ta
+    //disply no phone found
     const noPhoneFound = document.getElementById('no-found-message')
     if (phones.length === 0) {
         noPhoneFound.classList.remove('d-none')
@@ -27,9 +35,9 @@ const displyPhone = phones => {
 
 
     phones.forEach(phone => {
-        const phoneDiv = document.createElement('div')
-        phoneDiv.classList.add('col')
-        phoneDiv.innerHTML = `
+            const phoneDiv = document.createElement('div')
+            phoneDiv.classList.add('col')
+            phoneDiv.innerHTML = `
         <div class="card p-5 mt-5">
         <img src="${phone.image}" class="card-img-top" alt="...">
         <div class="card-body">
@@ -39,13 +47,32 @@ const displyPhone = phones => {
       </div>
         
         `
-        phoneContainer.appendChild(phoneDiv)
-    })
+            phoneContainer.appendChild(phoneDiv)
+        })
+        //stop loding toggler
+    togglespinner(false);
 }
-document.getElementById('search-btn').addEventListener('click', function() {
+const processSerach = (dataLimit) => {
+    togglespinner(true)
     const seaechFiled = document.getElementById('search-field')
     const seaechText = seaechFiled.value
-    lodePhone(seaechText)
+    lodePhone(seaechText, dataLimit)
+}
+document.getElementById('search-btn').addEventListener('click', function() {
+    //start loader
+    processSerach(10);
+
+})
+const togglespinner = isLoasing => {
+    const loadingsection = document.getElementById('lode-spinner')
+    if (isLoasing) {
+        loadingsection.classList.remove('d-none')
+    } else {
+        loadingsection.classList.add('d-none')
+    }
+}
+document.getElementById('bnt-show-all').addEventListener('click', function() {
+    processSerach()
 })
 
 //lodePhone()
